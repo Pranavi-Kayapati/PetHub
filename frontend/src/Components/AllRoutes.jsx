6a80596304a15240c2180a8f112a23590f1e5172
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-08-25 22:12:39 +0530

    push

--- frontend/src/Components/AllRoutes.jsx
+++ frontend/src/Components/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../Components/LandingPage/Pages/Home"
 import Signup from "./LoginSingupPage/Signup";
 import Login from "./LoginSingupPage/Login";
 import ProductPage from "./ProductPage/ProductPage";
 const AllRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/petPage" element={<ProductPage/>}/>
