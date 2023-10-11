a2ac26c56cb218a6dbd38bff24074caba97d8594
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-10-11 21:51:04 +0530

    Responsiveness and UI-changes made for landing Page

--- frontend/src/Components/LandingPage/Navbar/Navbar.jsx
+++ frontend/src/Components/LandingPage/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { CgChevronDown, CgChevronUp } from "react-icons/cg";
 // import { GiHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from "react-icons/gi";
 import {
   Menu,
   MenuButton,
@@ -10,159 +11,103 @@ import {
   Text,
   Box,
   useToast,
-  AvatarGroup,
-  Avatar,
+  useDisclosure,
   Button,
-  Toast,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  DrawerFooter,
 } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import "./Navbar.css"
 import { Link } from "react-router-dom";
 import { FaHeartCirclePlus, FaUserAlt } from "react-icons/fa";
 import logo from "../image/logo.png";
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [token, setToken] = useState("");
-  const [user, setUser] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = useRef()
   // const toast = useToast();
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  useEffect(() => {
-    const localToken = localStorage.getItem("token");
-    const email = localStorage.getItem("user");
-    setToken(localToken);
-    setUser(email);
-  }, [token, user]);
-
   // Handle logout function
-  let handleLogout = () => {
-    Toast({
-      title: "Logged Out",
-      status: "error",
-      duration: 2000,
-      isClosable: true,
-    });
-    localStorage.clear();
-    setToken("");
-    setUser("");
-  };
+  // let handleLogout = () => {
+  //   toast({
+  //     title: 'Logged Out',
+  //     status: 'error',
+  //     duration: 2000,
+  //     isClosable: true,
+  //   });
+  //   AuthLogoutFunc();
+  //   localStorage.clear();
+  // };
 
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        height: "80px",
-        position: "sticky",
-        zIndex: "5",
-        background: "white",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          marginLeft: "60px",
-          width: "35%",
-          justifyContent: "space-around",
-          color: "#3232af",
-          alignItems: "center",
-        }}
-      >
+    <div className="nav-main">
+      <div className="nav-left">
         <Link to="/">
           <img width="180px" src={logo} alt="" />
         </Link>
-        <h3 style={{ margin: "0 10px" }}>ALL ABOUT PETS</h3>
         <h3
+          className="about-pets hidden"
           onClick={toggleNav}
-          style={{ cursor: "pointer", fontSize: "20px", fontWeight: "bolder" }}
+          style={{ cursor: 'pointer', fontSize: '20px', fontWeight: 'bolder' }}
         >
+          <span>ALL ABOUT PETS</span>
           {isNavOpen ? <CgChevronUp /> : <CgChevronDown />}
         </h3>
-        <h3 style={{ margin: "0 10px" }}>
+        <h3 style={{ margin: '0 10px' }} className="hidden">
           <Link to="/services">OUR SERVICES</Link>
         </h3>
-        {/* <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "start",
-          }}
-        >
-          <h3 style={{ margin: "0 10px" }}>ALL ABOUT PETS</h3>
-          <h3
-            onClick={toggleNav}
-            style={{
-              cursor: "pointer",
-              fontSize: "20px",
-              fontWeight: "bolder",
-            }}
-          >
-            {isNavOpen ? <CgChevronUp /> : <CgChevronDown />}
-          </h3>
-        </div> */}
       </div>
 
-      <div style={{ display: "flex", alignItems: "center", width: "25%" }}>
+      <div className="nav-right" style={{ display: 'flex', alignItems: 'center', width: '25%' }}>
         <h3>
-          <AiFillHeart style={{ fontSize: "28px" }} />
+          <AiFillHeart style={{ fontSize: '28px' }} />
         </h3>
-
         <div
           style={{
-            borderLeft: "1px solid gray",
-            height: "50px",
-            margin: "0 8px",
+            borderLeft: '1px solid gray',
+            height: '50px',
+            margin: '0 8px',
           }}
         ></div>
-        {!token ? (
-          <>
-            <Link to="/signup">
-              <h3>Signup</h3>
-            </Link>
-            <Link to="/login">
-              <h3 style={{ marginLeft: "15px" }}>Login</h3>
-            </Link>
-          </>
-        ) : (
-          <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-              <AvatarGroup spacing="1rem">
-                <Avatar size="xs" bg="teal.500" />
-              </AvatarGroup>
-            </MenuButton>
-            <MenuList>
-              <MenuItem>{user}</MenuItem>
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        )}
+        <Link to="/signup">
+          <h3>Signup</h3>
+        </Link>
+        <Link to="/login">
+          <h3 style={{ marginLeft: '15px' }}>Login</h3>
+        </Link>
       </div>
 
       {isNavOpen && (
         <div
           style={{
-            position: "absolute",
-            top: "60px",
+            position: 'absolute',
+            top: '60px',
             left: 0,
-            width: "100%",
-            backgroundColor: "#3232af",
-            color: "#fff",
-            transition: "height 0.5s",
-            border: "1px solid red",
+            width: '100%',
+            backgroundColor: '#3232af',
+            color: '#fff',
+            transition: 'height 0.5s',
+            border: '1px solid red',
           }}
         >
           <ul
             style={{
-              listStyleType: "none",
+              listStyleType: 'none',
               padding: 0,
-              display: "flex",
-              justifyContent: "space-evenly",
-              height: "60px",
-              alignItems: "center",
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              height: '60px',
+              alignItems: 'center',
             }}
           >
             <li>
@@ -171,10 +116,10 @@ const Navbar = () => {
                   ADOPT OR GET INVOLVED
                 </MenuButton>
                 <MenuList color="#fff" bgColor="#3232af">
-                  <Link to="/pets">
+                  <Link to="/AboutPetfinder">
                     <MenuItem bgColor="#3232af">About petfinder</MenuItem>
                   </Link>
-                  <Link to="/pets">
+                  <Link to="/AdoptingPets">
                     <MenuItem bgColor="#3232af">Adopting Pets</MenuItem>
                   </Link>
                   <MenuItem bgColor="#3232af">
@@ -190,11 +135,11 @@ const Navbar = () => {
                   DOGS & PUPPIES
                 </MenuButton>
                 <MenuList color="#fff" bgColor="#3232af">
-                  <Link to="/pets">
+                  <Link to="/dogadopt">
                     <MenuItem bgColor="#3232af">Dog Adoption</MenuItem>
                   </Link>
                   <MenuItem bgColor="#3232af">Dog Breeds</MenuItem>
-                  <Link to="/pets">
+                  <Link to="/FeedingDog">
                     <MenuItem bgColor="#3232af">Feeding your Dog</MenuItem>
                   </Link>
                   {/* Add more dog-related menu items */}
@@ -204,9 +149,56 @@ const Navbar = () => {
           </ul>
         </div>
       )}
-      {/* <div className="hamburger-icon" onClick={toggleNav}>
-        <GiHamburgerMenu />
-      </div> */}
+      <div className="hamburger">
+        <GiHamburgerMenu ref={btnRef} size={"40px"} onClick={onOpen} />
+        <>
+          {/* <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button> */}
+          <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay
+            />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Create your account</DrawerHeader>
+
+              <DrawerBody>
+                <Link to="/signup">
+                  <h3>Signup</h3>
+                </Link>
+                <Link to="/login">
+                  <h3 style={{ marginLeft: '15px' }}>Login</h3>
+                </Link>
+              </DrawerBody>
+
+              <DrawerFooter>
+                <Button variant='outline' mr={3} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme='blue'>Save</Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </>
+      </div>
+      <div className="nav-footer">
+        <h3
+          className="about-pets"
+          onClick={toggleNav}
+          style={{ cursor: 'pointer', fontSize: '20px', fontWeight: 'bolder' }}
+        >
+          <span>ALL ABOUT PETS</span>
+          {isNavOpen ? <CgChevronUp /> : <CgChevronDown />}
+        </h3>
+        <h3 style={{ margin: '0 10px' }}>
+          <Link to="/services">OUR SERVICES</Link>
+        </h3>
+      </div>
     </div>
   );
 };
